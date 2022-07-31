fab31304099795d9a71d25b1db06452f9ebc0c87
Akhil Jharoria <95032356+Akhil-Jharoria@users.noreply.github.com>
2022-07-31 15:13:52 +0530

    Add files via upload

---
+++ MINCOINSREQ.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+ int T;
+ cin>>T;
+ 
+ while(T--)
+ {
+      int X;
+      cin>>X;
+      
+      int mincoin=X%10;
+      
+      cout<<mincoin<<endl;
+ }
+	return 0;
+}
